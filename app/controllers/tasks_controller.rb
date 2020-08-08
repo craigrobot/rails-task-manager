7592d3624f89df1b68a038a26ac1c9a1c232d973
Craig Horan <craighoran8@gmail.com>
2020-08-08 13:01:44 -0700

    Completed showing all task, one task, and new page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  def index #all tasks
+    @tasks = Task.all
+  end
+
+  def show #one task
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
 end
